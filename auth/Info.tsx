07c9fc61797581ac82ee8b28578c2248869f907c
unknown <gojh1013@gmail.com>
2024-02-19 22:22:25 +0900

    style: 드롭다운 아이콘 및 선택시 글자 색

--- auth/Info.tsx
+++ auth/Info.tsx
@@ -1,10 +1,10 @@
 import React, { useState, useCallback } from 'react';
-import { View, StyleSheet, TouchableOpacity, Text, ScrollView } from 'react-native';
+import { View, StyleSheet, TouchableOpacity, Text, ScrollView, TouchableWithoutFeedback } from 'react-native';
 import { Input, Button, Header } from 'react-native-elements';
 import Icons from 'react-native-vector-icons/MaterialIcons';
 import { NavigationProp, useNavigation } from '@react-navigation/native';
 import {
-    Department,
+    UNI,
     Humanity,
     NaturalScience,
     SocialScience,
@@ -26,6 +26,7 @@ interface InfoProps {
 
 const Info: React.FC<InfoProps> = ({ navigation }) => {
     const [nickName, setNickName] = useState<string>('');
+    const [uni, setUni] = useState<string | null>(null);
     const [depart, setDepart] = useState<string | null>(null);
     const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
 
@@ -42,55 +43,68 @@ const Info: React.FC<InfoProps> = ({ navigation }) => {
     };
 
     const handleDropdownSelect = (value: string) => {
-        setDepart(value);
+        setUni(value);
+        setIsDropdownOpen(false);
+    };
+
+    const handleBlur = () => {
         setIsDropdownOpen(false);
     };
 
     return (
-        <View>
-            <Header
-                containerStyle={{
-                    borderBottomWidth: 0,
-                    backgroundColor: 'white',
-                    marginTop: 20,
-                    alignItems: 'center',
-                }}
-                backgroundColor="white"
-                barStyle="default"
-                centerComponent={{
-                    text: '정보입력',
-                    style: { color: 'black', fontSize: 34, fontWeight: 'bold' },
-                }}
-                leftComponent={
-                    <TouchableOpacity onPress={handleGoBack}>
-                        <Icons name="arrow-back-ios" size={25} style={{ color: '#BDBDBD' }} />
+        <TouchableWithoutFeedback onPress={handleBlur}>
+            <View>
+                <Header
+                    containerStyle={{
+                        borderBottomWidth: 0,
+                        backgroundColor: 'white',
+                        marginTop: 20,
+                        alignItems: 'center',
+                    }}
+                    backgroundColor="white"
+                    barStyle="default"
+                    centerComponent={{
+                        text: '정보입력',
+                        style: { color: 'black', fontSize: 34, fontWeight: 'bold' },
+                    }}
+                    leftComponent={
+                        <TouchableOpacity onPress={handleGoBack}>
+                            <Icons name="arrow-back-ios" size={25} style={{ color: '#BDBDBD' }} />
+                        </TouchableOpacity>
+                    }
+                    leftContainerStyle={{ flex: 1, justifyContent: 'center' }}
+                />
+                <View style={{ marginTop: 32 }}>
+                    <Input
+                        containerStyle={[styles.inputContainer]}
+                        onChangeText={onChangeNickname}
+                        placeholder="닉네임"
+                    />
+                </View>
+                <View style={{ marginTop: 32 }}>
+                    <TouchableOpacity onPress={handleDropdownToggle} style={styles.dropdownToggle}>
+                        <Text style={[styles.dropdownText, { color: uni ? 'black' : '#ADB3BC' }]}>
+                            {uni || '학부 선택'}
+                        </Text>
+                        <Icons name="arrow-drop-down" size={25} style={{ color: '#BDBDBD' }} />
                     </TouchableOpacity>
-                }
-                leftContainerStyle={{ flex: 1, justifyContent: 'center' }}
-            />
-            <View style={{ marginTop: 32 }}>
-                <Input containerStyle={[styles.inputContainer]} onChangeText={onChangeNickname} placeholder="닉네임" />
-            </View>
-            <View style={{ marginTop: 32 }}>
-                <TouchableOpacity onPress={handleDropdownToggle} style={styles.dropdownToggle}>
-                    <Text style={styles.dropdownText}>{depart || '학과를 선택해주세요'}</Text>
-                </TouchableOpacity>
-                {isDropdownOpen && (
-                    <ScrollView style={styles.dropdownList}>
-                        {Department.map((item) => (
-                            <TouchableOpacity
-                                key={item.value}
-                                style={styles.dropdownItem}
-                                onPress={() => handleDropdownSelect(item.value)}
-                            >
-                                <Text>{item.label}</Text>
-                            </TouchableOpacity>
-                        ))}
-                    </ScrollView>
-                )}
-            </View>
 
-            {/* <Button
+                    {isDropdownOpen && (
+                        <ScrollView style={styles.dropdownList}>
+                            {UNI.map((item) => (
+                                <TouchableOpacity
+                                    key={item.value}
+                                    style={styles.dropdownItem}
+                                    onPress={() => handleDropdownSelect(item.value)}
+                                >
+                                    <Text>{item.label}</Text>
+                                </TouchableOpacity>
+                            ))}
+                        </ScrollView>
+                    )}
+                </View>
+
+                {/* <Button
                 buttonStyle={{ width: 330, marginTop: 170, borderRadius: 100, height: 50 }}
                 containerStyle={{ margin: 5, alignItems: 'center', justifyContent: 'center' }}
                 disabledStyle={{
@@ -104,7 +118,8 @@ const Info: React.FC<InfoProps> = ({ navigation }) => {
                 titleProps={{}}
                 titleStyle={{ textAlign: 'center' }}
             /> */}
-        </View>
+            </View>
+        </TouchableWithoutFeedback>
     );
 };
 
@@ -122,11 +137,14 @@ const styles = StyleSheet.create({
         backgroundColor: '#F6F6F6',
         color: 'ADB3BC',
         height: 50,
-        borderColor: 'gray',
-        borderWidth: 0.5,
         borderRadius: 8,
         paddingHorizontal: 8,
-        justifyContent: 'center',
+        justifyContent: 'space-between',
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        borderColor: '#E8E8E8',
+        borderWidth: 1,
     },
     dropdownText: {
         fontSize: 16,
---
+++ data/department.ts
@@ -0,0 +1,120 @@
+interface Department {
+    label: string;
+    value: string;
+}
+export const UNI: Department[] = [
+    { label: '인문대학', value: '인문대학' },
+    { label: '자연과학대학', value: '자연과학대학' },
+    { label: '사회과학대학', value: '사회과학대학' },
+    { label: '글로벌정경대학', value: '글로벌정경대학' },
+    { label: '공과대학', value: '공과대학' },
+    { label: '정보기술대학', value: '정보기술대학' },
+    { label: '경영대학', value: '경영대학' },
+    { label: '예술체육대학', value: '예술체육대학' },
+    { label: '사범대학', value: '사범대학' },
+    { label: '도시과학대학', value: '도시과학대학' },
+    { label: '생명과학기술대학', value: '생명과학기술대학' },
+    { label: '동북아국제통상학부', value: '동북아국제통상학부' },
+    { label: '법학부', value: '법학부' },
+];
+
+export const Humanity: Department[] = [
+    { label: '국어국문학과', value: '국어국문학과' },
+    { label: '영어영문학과', value: '영어영문학과' },
+    { label: '독어독문학과', value: '독어독문학과' },
+    { label: '불어불문학과', value: '불어불문학과' },
+    { label: '일본지역문화학과', value: '일본지역문화학과' },
+    { label: '중어중국학과', value: '중어중국학과' },
+];
+
+export const NaturalScience: Department[] = [
+    { label: '수학과', value: '수학과' },
+    { label: '물리학과', value: '물리학과' },
+    { label: '화학과', value: '화학과' },
+    { label: '패션산업학과', value: '패션산업학과' },
+    { label: '해양학과', value: '해양학과' },
+];
+
+export const SocialScience: Department[] = [
+    { label: '사회복지학과', value: '사회복지학과' },
+    { label: '미디어커뮤니케이션학과', value: '미디어커뮤니케이션학과' },
+    { label: '문헌정보학과', value: '문헌정보학과' },
+    { label: '창의인재개발학과', value: '창의인재개발학과' },
+];
+
+export const GlobalEconomics: Department[] = [
+    { label: '행정학과', value: '행정학과' },
+    { label: '정치외교학과', value: '정치외교학과' },
+    { label: '경제학과', value: '경제학과' },
+    { label: '무역학부', value: '무역학부' },
+    { label: '소비자학과', value: '소비자학과' },
+];
+
+export const Engineering: Department[] = [
+    { label: '기계공학과', value: '기계공학과' },
+    { label: '전기공학과', value: '전기공학과' },
+    { label: '전자공학과', value: '전자공학과' },
+    { label: '산업경영공학과', value: '산업경영공학과' },
+    { label: '신소재공학과', value: '신소재공학과' },
+    { label: '안전공학과', value: '안전공학과' },
+    { label: '에너지화학공학과', value: '에너지화학공학과' },
+    { label: '바이오-로봇 시스템 공학과', value: '바이오-로봇 시스템 공학과' },
+];
+
+export const InformationTechnology: Department[] = [
+    { label: '컴퓨터공학부', value: '컴퓨터공학부' },
+    { label: '정보통신공학과', value: '정보통신공학과' },
+    { label: '임베디드시스템공학과', value: '임베디드시스템공학과' },
+];
+
+export const Business: Department[] = [
+    { label: '경영학부', value: '경영학부' },
+    { label: '데이터과학과', value: '데이터과학과' },
+    { label: '세무회계학과', value: '세무회계학과' },
+    { label: '테크노경영학과', value: '테크노경영학과' },
+];
+
+export const ArtPhysical: Department[] = [
+    { label: '한국화전공(조형예술학부)', value: '한국화전공(조형예술학부)' },
+    { label: '서양화전공(조형예술학부)', value: '서양화전공(조형예술학부)' },
+    { label: '디자인학부', value: '디자인학부' },
+    { label: '공연예술학과', value: '공연예술학과' },
+    { label: '스포츠과학부', value: '스포츠과학부' },
+    { label: '운동건강학부', value: '운동건강학부' },
+];
+
+export const Education: Department[] = [
+    { label: '국어교육과', value: '국어교육과' },
+    { label: '영어교육과', value: '영어교육과' },
+    { label: '일어교육과', value: '일어교육과' },
+    { label: '수학교육과', value: '수학교육과' },
+    { label: '체육교육과', value: '체육교육과' },
+    { label: '유아교육과', value: '유아교육과' },
+    { label: '역사교육과', value: '역사교육과' },
+    { label: '윤리교육과', value: '윤리교육과' },
+];
+
+export const UrbanScience: Department[] = [
+    { label: '도시행정학과', value: '도시행정학과' },
+    { label: '건설환경공학', value: '건설환경공학' },
+    { label: '환경공학', value: '환경공학' },
+    { label: '도시공학과', value: '도시공학과' },
+    { label: '건축공학', value: '건축공학' },
+    { label: '도시건축학', value: '도시건축학' },
+];
+
+export const LifeScience: Department[] = [
+    { label: '생명과학부(생명과학전공)', value: '생명과학부(생명과학전공)' },
+    { label: '생명과학부(분자의생명전공)', value: '생명과학부(분자의생명전공)' },
+    { label: '생명공학부(생명공학전공)', value: '생명공학부(생명공학전공)' },
+    { label: '생명공학부(나노바이오공학전공)', value: '생명공학부(나노바이오공학전공)' },
+];
+
+export const NortheastAsia: Department[] = [
+    { label: '동북아국제통상학부', value: '동북아국제통상학부' },
+    { label: '동북아국제통상전공', value: '동북아국제통상전공' },
+    { label: '스마트물류공학전공', value: '스마트물류공학전공' },
+    { label: 'IBE전공', value: 'IBE전공' },
+];
+
+export const Law: Department[] = [{ label: '법학부', value: '법학부' }];
