01379698c64d3a9de3c8102f208d5cccc6476359
unknown <gojh1013@gmail.com>
2024-03-07 22:40:30 +0900

    chore: tsconfig 수정

--- pages/mypage.tsx
+++ pages/mypage.tsx
@@ -1,8 +1,9 @@
-import React, { ChangeEvent, useState, useEffect, useCallback } from 'react';
+import { ChangeEvent, useState, useEffect, useCallback } from 'react';
 import { Input, InputProps, Button, Header } from 'react-native-elements';
 import Icons from 'react-native-vector-icons/MaterialIcons';
 import { TextStyle, ViewStyle, StyleSheet, View, TouchableOpacity } from 'react-native';
 import { NavigationProp, useNavigation } from '@react-navigation/native';
+import React from 'react';
 
 interface MypageProps {
     navigation: NavigationProp<any>;
