cbf98348cc44615fbc080fc164ae5792e81a240d
unknown <gojh1013@gmail.com>
2024-03-08 23:21:45 +0900

    feat: mypage 생성

--- pages/mypage.tsx
+++ pages/mypage.tsx
@@ -9,8 +9,34 @@ interface MypageProps {
     navigation: NavigationProp<any>;
 }
 
-const Mypage: React.FC<MypageProps> = () => {
-    return <View></View>;
+const Mypage: React.FC<MypageProps> = ({ navigation }) => {
+    const handleGoBack = () => {
+        navigation.goBack();
+    };
+    return (
+        <View>
+            <Header
+                containerStyle={{
+                    borderBottomWidth: 0,
+                    backgroundColor: 'white',
+                    marginTop: 20,
+                    alignItems: 'center',
+                }}
+                backgroundColor="white"
+                barStyle="default"
+                centerComponent={{
+                    text: '내 정보',
+                    style: { color: '#1B1B1B', fontSize: 34, fontWeight: 'bold' },
+                }}
+                leftComponent={
+                    <TouchableOpacity onPress={handleGoBack}>
+                        <Icons name="arrow-back-ios" size={25} style={{ color: '#BDBDBD' }} />
+                    </TouchableOpacity>
+                }
+                leftContainerStyle={{ flex: 1, justifyContent: 'center' }}
+            />
+        </View>
+    );
 };
 
 export default Mypage;
