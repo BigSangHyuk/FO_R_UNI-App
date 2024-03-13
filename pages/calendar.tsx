91f385e81d4bf1833624962d9aa680b9546594e4
고재현 <gojh1013@gmail.com>
2024-03-13 20:02:17 +0900

    chore: react-native-calendar설치

---
+++ pages/calendar.tsx
@@ -0,0 +1,5 @@
+import React, { useState, useRef } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, Animated, FlatList } from 'react-native';
+import { Header } from 'react-native-elements';
+import Icons from 'react-native-vector-icons/MaterialIcons';
+import { NavigationProp, useNavigation } from '@react-navigation/native';
