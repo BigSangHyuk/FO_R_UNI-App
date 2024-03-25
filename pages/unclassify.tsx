5bc57caebefe57aa238d56bf345bab3d183d7439
고재현 <gojh1013@gmail.com>
2024-03-25 19:40:10 +0900

    feat: 마분류글 최대 auto 설정

--- pages/unclassify.tsx
+++ pages/unclassify.tsx
@@ -1,33 +1,16 @@
 import React, { FC, useState } from 'react';
 import { View, Text, StyleSheet, Switch, TextInput, FlatList } from 'react-native';
 import { Header, Button } from 'react-native-elements';
-import Icons from 'react-native-vector-icons/MaterialIcons';
 
 const UnClassify: FC = () => {
     const comment = [
         { title: 'Item 5', duration: '3월 11일' },
         { title: 'Item 6', duration: '60min' },
         { title: 'Item 7', duration: '30min' },
-        { title: 'Item 8', duration: '15min' },
-        { title: 'Item 8', duration: '15min' },
-        { title: 'Item 8', duration: '15min' },
-        { title: 'Item 8', duration: '15min' },
-        { title: 'Item 8', duration: '15min' },
-        { title: 'Item 8', duration: '15min' },
-        { title: 'Item 8', duration: '15min' },
-        { title: 'Item 8', duration: '15min' },
-        { title: 'Item 8', duration: '15min' },
-        { title: 'Item 8', duration: '15min' },
-        { title: 'Item 8', duration: '15min' },
-        { title: 'Item 8', duration: '15min' },
-        { title: 'Item 8', duration: '15min' },
-        { title: 'Item 8', duration: '15min' },
-        { title: 'Item 8', duration: '15min' },
-        { title: 'Item 8', duration: '15min' },
-        { title: 'Item 8', duration: '15min' },
+        { title: 'Item 7', duration: '30min' },
     ];
     return (
-        <View>
+        <View style={styles.container}>
             <Header
                 containerStyle={{
                     borderBottomWidth: 0,
@@ -42,37 +25,31 @@ const UnClassify: FC = () => {
                     style: { color: '#1B1B1B', fontSize: 34, fontWeight: 'bold' },
                 }}
             />
-            <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-                <View style={styles.listContainer}>
-                    <FlatList
-                        data={comment}
-                        renderItem={({ item }) => (
-                            <View style={styles.item}>
-                                <Text style={styles.itemTitle}>○ {item.title}</Text>
-                                <Text style={styles.itemDuration}>{item.duration}</Text>
-                            </View>
-                        )}
-                        keyExtractor={(item) => item.title}
-                    />
-                </View>
+            <View style={styles.listContainer}>
+                <FlatList
+                    data={comment}
+                    renderItem={({ item }) => (
+                        <View style={styles.item}>
+                            <Text style={styles.itemTitle}>○ {item.title}</Text>
+                            <Text style={styles.itemDuration}>{item.duration}</Text>
+                        </View>
+                    )}
+                    keyExtractor={(item) => item.title}
+                />
             </View>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
     listContainer: {
-        width: '100%',
-        height: 354,
+        flex: 1,
+        maxHeight: '70%',
         backgroundColor: '#F6F6F6',
         marginTop: 32,
-    },
-    list: {
-        width: 323,
-        height: 35,
-        alignItems: 'center',
-        justifyContent: 'center',
-        fontSize: 16,
     },
     item: {
         flexDirection: 'row',
