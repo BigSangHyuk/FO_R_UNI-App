e7df78430fb32d078fcf9f9759b09d633547b35c
unknown <gojh1013@gmail.com>
2024-04-22 01:29:36 +0900

    feat: 상세정보 모달창

--- pages/unclassify.tsx
+++ pages/unclassify.tsx
@@ -1,5 +1,5 @@
 import React, { FC, useState, useEffect } from 'react';
-import { View, Text, StyleSheet, FlatList, TouchableWithoutFeedback } from 'react-native';
+import { View, Text, StyleSheet, FlatList, TouchableWithoutFeedback, Modal, TouchableOpacity } from 'react-native';
 import { Header } from 'react-native-elements';
 import Http from '../address/backend_url';
 import { getStorage } from '../auth/asyncstorage';
@@ -33,7 +33,7 @@ const UnClassify: FC = () => {
         unclassified();
     }, []);
 
-    const fetchPostDetails = async (postId) => {
+    const fecthPostDetails = async (postId: number) => {
         const accessToken = await getStorage('accessToken');
         const res = await fetch(Http + `/posts/${postId}`, {
             method: 'GET',
@@ -79,6 +79,32 @@ const UnClassify: FC = () => {
                 keyExtractor={(item) => item.postId.toString()}
                 contentContainerStyle={styles.listContainer}
             />
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={() => {
+                    setModalVisible(!modalVisible);
+                }}
+            >
+                <View style={styles.centeredView}>
+                    <View style={styles.modalView}>
+                        {selectedPost ? (
+                            <>
+                                <Text style={styles.modalText}></Text>
+                            </>
+                        ) : (
+                            <Text>불러오는중...</Text>
+                        )}
+                        <TouchableOpacity
+                            style={[styles.button, styles.buttonClose]}
+                            onPress={() => setModalVisible(!modalVisible)}
+                        >
+                            <Text style={styles.textStyle}>닫기</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </Modal>
         </View>
     );
 };
@@ -102,6 +128,47 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         flex: 1,
         marginRight: 10,
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 22,
+    },
+    modalView: {
+        margin: 20,
+        backgroundColor: 'white',
+        borderRadius: 20,
+        padding: 35,
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+    },
+    button: {
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2,
+    },
+    buttonOpen: {
+        backgroundColor: '#F194FF',
+    },
+    buttonClose: {
+        backgroundColor: '#2196F3',
+    },
+    textStyle: {
+        color: 'white',
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    modalText: {
+        marginBottom: 15,
+        textAlign: 'center',
     },
 });
 export default UnClassify;
